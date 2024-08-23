52ebbb4d4a88d07eb22032f2cadcbb4f9ccd9cc0
Neroxsis <helbing.dominik@gmail.com>
2024-08-23 10:40:55 +0200

    Add documentation and comments

--- src/bmArrayUtility/bmCol.m
+++ src/bmArrayUtility/bmCol.m
@@ -1,8 +1,28 @@
-% Bastien Milani
-% CHUV and UNIL
-% Lausanne - Switzerland
-% May 2023
-
 function out = bmCol(a)
+% out = bmCol(a)
+%
+% This function returns the input data as a column vector.
+% This is the same as doing:
+%   temp = a;
+%   out = a(:);
+% But allows to do it in one line and thus nest in other function calls.
+%
+% Authors:
+%   Bastien Milani
+%   CHUV and UNIL
+%   Lausanne - Switzerland
+%   May 2023
+%
+% Contributors:
+%   Dominik Helbing (Documentation & Comments)
+%   MattechLab 2024
+%
+% Parameters:
+%   a (array): Input data
+%
+% Returns:
+%   out (column vector): Data (a) as column vector
+
 out = a(:);
+
 end
