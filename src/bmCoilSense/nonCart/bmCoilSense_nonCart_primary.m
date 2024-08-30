d8d1be2cd52967b7a4c90675e26758f902cfddfe
Neroxsis <helbing.dominik@gmail.com>
2024-08-30 12:52:50 +0200

    Add documentation & comments (coil sensitivity estimation)

--- src/bmCoilSense/nonCart/bmCoilSense_nonCart_primary.m
+++ src/bmCoilSense/nonCart/bmCoilSense_nonCart_primary.m
@@ -1,69 +1,114 @@
-% Bastien Milani
-% CHUV and UNIL
-% Lausanne - Switzerland
-% May 2023
-
 function C = bmCoilSense_nonCart_primary(y, y_ref, C_ref, Gn, ve, m)
-% y (nPt,nCh)
-% y_ref data of the selected body coil (nPt,1)
-% C_ref Csensitivity of the selected body coil (same size as image)
-% Gn
-% ve size of (1, nPt)
-% m size of the image
-nIter_smooth = 2; % ----------------------------------------------------------- magic number
-L_nIter = 1000; % ------------------------------------------------------------- magic number
-L_th = 1e-4; % ---------------------------------------------------------------- magic number
+% C = bmCoilSense_nonCart_primary(y, y_ref, C_ref, Gn, ve, m)
+%
+% This function estimates the coil sensitivity of all surface coils using
+% the coil sensitvity and data of the reference coil. Using the mask
+% that indicates the good pixels of the data, a Laplace solver algorithm is
+% used to estimate the coil senstivity of the surface coils for the masked
+% parts (m = 0).
+%
+% Authors:
+%   Bastien Milani
+%   CHUV and UNIL
+%   Lausanne - Switzerland
+%   May 2023
+%
+% Contributors:
+%   Dominik Helbing (Documentation & Comments)
+%   MattechLab 2024
+%
+% Parameters:
+%   y (array): The data of the surface coils. Has the size (nPt, nCh).
+%   y_ref (array): The column vector containing the data of the reference
+%    coil.
+%   C_ref (array): The coil sensitivity of the reference coil. Has the size
+%    of Gn.N_u (block format).
+%   Gn (bmSparseMat): The sparse matrix used to grid the non-uniform data
+%    to a uniform grid.
+%   ve (array): The volume elements for each point in y. Can be the size of
+%    one channel, a scalar or the size of y.
+%   m (array): The mask that masks the data that is not usefull with m = 0.
+%    Has the size of one channel in block format.
+%
+% Returns:
+%   C (array): The coil sensitvity of all surface coils (channels) in block
+%    format.
 
+
+%% Initialize arguments
+% Magic numbers
+nIter_smooth = 2;
+L_nIter = 1000;
+L_th = 1e-4;
+
+% Extract "hidden" variables from arguments
 N_u         = double(Gn.N_u(:)');
 nPt         = double(Gn.r_size);
 imDim       = size(N_u(:), 1);  
 nCh         = size(y(:), 1)/nPt;
 
 nCh_array   = size(y, 2);  
+
+% Set y_ref to the same scale as y (make them comparable)
 y_ref       = nCh_array*y_ref/bmY_norm(y_ref, ve)*mean(bmCol(bmY_norm(y, ve, false))); 
 
-x_ref       = bmBlockReshape(bmNasha(y_ref, Gn, N_u), N_u);
+
+% Compute the regridded data in the image space for the refrence body coil
+% in block format 
+x_ref       = bmBlockReshape(bmNasha(y_ref, Gn, N_u), N_u); 
+
+% Compute the regridded data in the image space for all surface coils in
+% block format 
 x           = bmBlockReshape(bmNasha(y, Gn, N_u),     N_u); 
 
 
-
+% Have the mask in block format and get its negative
 m           = logical(bmBlockReshape(m, N_u)); 
 m_neg       = not(m); 
 
+% Calculate the estimated anatomical image from the reference coil and set
+% masked parts to 1
 anat_ref    = x_ref./C_ref; 
 anat_ref(m_neg) = 1; 
 
-
+% Initialize complex array for coil sensitvity of each surface coil
 z = zeros([N_u, nCh], 'single'); 
 C = complex(z, z); 
 
-% For each (surface coil)
-% channel compute the coil sensitivity (in and out of the mask)
+
+%% Compute coil sensitivity
+% For each channel (surface coil) channel compute the coil sensitivity
 for i = 1:nCh    
-    if imDim == 1
+    if imDim == 1 % See imDim == 3 for comments
         temp_im = x(:, i);
-        % Smoothing with masked (only good pixel contributes) Pseudodiffusion
         temp_im = bmImPseudoDiffusion_inMask(temp_im./anat_ref, m, nIter_smooth); 
         temp_im(m_neg) = 0;
-        % Solve a laplacian solver to estimate the coil sensitivity outside
-        % of the mask
         C(:, i) = bmImLaplaceEquationSolver(temp_im, m, L_nIter, L_th, 'omp');
         
-    elseif imDim == 2
+    elseif imDim == 2 % See imDim == 3 for comments
         temp_im = x(:, :, i);  
         temp_im = bmImPseudoDiffusion_inMask(temp_im./anat_ref, m, nIter_smooth);
         temp_im(m_neg) = 0; 
         C(:, :, i) = bmImLaplaceEquationSolver(temp_im, m, L_nIter, L_th, 'omp');
 
-    elseif imDim == 3
-        temp_im = x(:, :, :, i);  
+    elseif imDim == 3 
+        % Get data of a channel
+        temp_im = x(:, :, :, i); 
+        
+        % Diffuse magnitude of unmasked data (m = 1) by averaging over
+        % neighbors, with the data devided by the image estimate
+        % C_coil = I_coil / I_real
         temp_im = bmImPseudoDiffusion_inMask(temp_im./anat_ref, m, nIter_smooth);
+
+        % Solve a Laplacian solver to estimate the coil sensitivity for the
+        % masked data (smooth masked parts)
         temp_im(m_neg) = 0;
         C(:, :, :, i) = bmImLaplaceEquationSolver(temp_im, m, L_nIter, L_th, 'omp');
         
     end
 end
 
+% Scale and reshape the coil sensitvity to block format
 C = C*nCh_array; 
 C = bmBlockReshape(C, N_u); 
 
--- src/bmCoilSense/nonCart/bmCoilSense_nonCart_ref.m
+++ src/bmCoilSense/nonCart/bmCoilSense_nonCart_ref.m
@@ -1,98 +1,160 @@
-% Bastien Milani
-% CHUV and UNIL
-% Lausanne - Switzerland
-% May 2023
-
 function [y_ref, C_ref] = bmCoilSense_nonCart_ref(y, Gn, m, nSmooth_phi)
-% We need to chose the data of one of the body coils, the scope of this
-% function is to select one of the body coil data, and estimate the coil
-% sensitivity of that body coil.
-% y (complex) in col shape (nPt, nCh)
-% Gn (Sparse matrix)
-% m (_,_),(_,_,_) size of one frame
-% nSmooth_phi (1)
+% [y_ref, C_ref] = bmCoilSense_nonCart_ref(y, Gn, m, nSmooth_phi)
+%
+% This function uses the data given in y to create a reference that can be
+% used to estimate the coil sensitvity of the surface coils. Using the mask
+% that indicates the good pixels of the data, a Laplace solver algorithm is
+% used to estimate the coil senstivity of the reference coil for the masked
+% parts (m = 0).
+% 
+% Authors:
+%   Bastien Milani
+%   CHUV and UNIL
+%   Lausanne - Switzerland
+%   May 2023
+%
+% Contributors:
+%   Dominik Helbing (Documentation & Comments)
+%   MattechLab 2024
+%
+% Parameters:
+%   y (array): The acquired data of at least one reference coil. Complex
+%    and in col format (nPt, nCh).
+%   Gn (bmSparseMat): Sparse matrix of class bmSparseMat that grids the
+%    non-uniform data (y) onto a uniform grid. 
+%   m (array): A mask of the same size as the datapoints in a channel,
+%    which indicates the good data (pixels) with a 1 (or true).
+%   nSmooth_phi (int): The number of iterations for pseudo diffusing the
+%    complex data, resulting in the coil sensitvity being a complex array.
+%    If this is not wanted (WHICH IT IS FOR ALL USES ATM) please use [].
+%
+% Returns:
+%   y_ref (list): Column vector containing the acquired data in k-space of
+%    the reference coil chosen.
+%   C_ref (array): The estimated coil sensitvity of the chosen reference
+%    coil, given in block format.
+%
+% Notes:
+%   The data in y should be acquired using body coils.
+%   The assumption can be made that C_ref is more uniform and can be
+%   estimated by diffusing and smoothing the data devided by the RMS
+%   instead of the image (C_ref = I_ref / I_anat, I_anat assumed to be
+%   similar to RMS)
 
-L_nIter = 1000; % ----------------------------------------------------------- magic number
-L_th    = 1e-4; % ----------------------------------------------------------- magic number
+%% Initialize arguments
+% Magic numbers
+L_nIter = 1000;
+L_th    = 1e-4;
+nIter_smooth = 2;
 
+% Extract "hidden" variables from arguments
 N_u         = double(Gn.N_u(:)');
 nPt         = double(Gn.r_size); 
 imDim       = size(N_u(:), 1);
-nCh         = size(y(:), 1)/nPt; 
+nCh         = size(y(:), 1)/nPt;
 
-m           = logical(reshape(m, N_u));
+% Prepare masks for one channel
+m           = logical(reshape(m, N_u)); 
 m_neg       = not(m); 
+
+% Change mask to column format and repeat for every channel
 m_rep       = repmat(m(:), [1, nCh]); 
 m_neg_rep   = not(m_rep); 
 
+
+%% Compute data
+% Compute the regridded data in the image space for the reference coils
+% in the column format and set the masked parts to 1
 x_ch            = single(bmColReshape(bmNasha(y, Gn, N_u), N_u)); 
-x_ch(m_neg_rep) = 1; 
-x_ch            = bmBlockReshape(x_ch, N_u);
-myRMS           = bmRMS(x_ch, N_u);
+x_ch(m_neg_rep) = 1;
+
+% Change data to block format
+x_ch            = bmBlockReshape(x_ch, N_u); 
+
+% Calculate the RMS across all channels -> Assume to be close to the actual
+% anatomical image
+myRMS           = bmRMS(x_ch, N_u); 
 
 
-z     = zeros([N_u, nCh], 'single'); 
+% Initialize complex array for coil sensitvity of each reference coil
+z     = zeros([N_u, nCh], 'single');
 C     = complex(z, z);
 
-for i = 1:nCh
-    if imDim == 1
-        
-        temp_a              = x_ch(:, i);
-        C_abs               = bmImPseudoDiffusion_inMask(  abs(temp_a)./myRMS  , m, 2); 
-        
-        if not(isempty(nSmooth_phi))
-            C_phi           = angle(bmImPseudoDiffusion_inMask(temp_a,    m, nSmooth_phi));
-        else
-            C_phi           = zeros(size(temp_a)); 
-        end
-        
-        temp_C              = C_abs.*exp(1i*C_phi);
-        temp_C(m_neg)       = 0; 
-        C(:, i)             = bmImLaplaceEquationSolver(temp_C, m, L_nIter, L_th, 'omp'); 
 
-    elseif imDim == 2
+%% Compute coil sensitivity of refrence coils
+for i = 1:nCh
+    if imDim == 1 % See imDim == 3 for comments
         
-        temp_a              = x_ch(:, :, i);
-        C_abs               = bmImPseudoDiffusion_inMask(  abs(temp_a)./myRMS  , m, 2); 
+        temp_a = x_ch(:, i);
+        C_abs = bmImPseudoDiffusion_inMask(abs(temp_a)./myRMS, m, nIter_smooth); 
         
         if not(isempty(nSmooth_phi))
-            C_phi           = angle(bmImPseudoDiffusion_inMask(temp_a,    m, nSmooth_phi));
+            C_phi = angle(bmImPseudoDiffusion_inMask(temp_a, m, nSmooth_phi));
         else
-            C_phi           = zeros(size(temp_a)); 
+            C_phi = zeros(size(temp_a)); 
         end
         
-        temp_C              = C_abs.*exp(1i*C_phi); 
-        temp_C(m_neg)       = 0; 
-        C(:, :, i)          = bmImLaplaceEquationSolver(temp_C, m, L_nIter, L_th, 'omp');  
+        temp_C = C_abs.*exp(1i*C_phi);
+        temp_C(m_neg) = 0; 
+        C(:, i) = bmImLaplaceEquationSolver(temp_C, m, L_nIter, L_th, 'omp'); 
+
+    elseif imDim == 2 % See imDim == 3 for comments
+        
+        temp_a = x_ch(:, :, i);
+        C_abs = bmImPseudoDiffusion_inMask(abs(temp_a)./myRMS, m, nIter_smooth); 
+        
+        if not(isempty(nSmooth_phi))
+            C_phi = angle(bmImPseudoDiffusion_inMask(temp_a, m, nSmooth_phi));
+        else
+            C_phi = zeros(size(temp_a)); 
+        end
+        
+        temp_C = C_abs.*exp(1i*C_phi); 
+        temp_C(m_neg) = 0; 
+        C(:, :, i) = bmImLaplaceEquationSolver(temp_C, m, L_nIter, L_th, 'omp');  
         
     elseif imDim == 3
-        
-        temp_a              = x_ch(:, :, :, i);
-        C_abs               = bmImPseudoDiffusion_inMask(  abs(temp_a)./myRMS  , m, 2); 
+        % Get data of one channel
+        temp_a = x_ch(:, :, :, i);
+
+        % Diffuse magnitude of unmasked data (m = 1) by averaging over
+        % neighbors, with the data devided by the image estimate (RMS)
+        % C_ref = I_ref / I_real
+        C_abs = bmImPseudoDiffusion_inMask(abs(temp_a)./myRMS, m, nIter_smooth); 
 
         if not(isempty(nSmooth_phi))
-            C_phi           = angle(bmImPseudoDiffusion_inMask(temp_a,    m, nSmooth_phi));
+            % Get the angle of smoothed data if nSmooth_phi is not empty
+            C_phi = angle(bmImPseudoDiffusion_inMask(temp_a, m, nSmooth_phi));
         else
-            C_phi           = zeros(size(temp_a)); 
+            % Use zeros otherwise (real number)
+            C_phi = zeros(size(temp_a)); 
         end
         
-        temp_C              = C_abs.*exp(1i*C_phi); 
-        temp_C(m_neg)       = 0; 
-        C(:, :, :, i)       = bmImLaplaceEquationSolver(temp_C, m, L_nIter, L_th, 'omp'); 
+        % Return back to a complex number if C_phi is not zero and set
+        % masked parts to zero
+        temp_C = C_abs.*exp(1i*C_phi); 
+        temp_C(m_neg) = 0;
+        
+        % Solve a Laplacian solver to estimate the coil sensitivity for the
+        % masked data (smooth masked parts)
+        C(:, :, :, i) = bmImLaplaceEquationSolver(temp_C, m, L_nIter, L_th, 'omp');
 
     end
 end
 
+
+%% Select and return reference data and coil sensitivity
+% Reshape data and coil sensitvity to column format
 x_ch   = bmColReshape(x_ch, N_u); 
 C      = bmColReshape(C, N_u); 
 
 
-% definition of C_ref and y_ref -------------------------------------------
+% C_ref and y_ref only take data of one reference coil
 C_ref = C(:, 1); 
 y_ref = y(:, 1); 
-% END_definition of C_ref and y_ref ---------------------------------------
 
 
+% Reshape reference coil sensitivity to column format
 C_ref = bmBlockReshape(C_ref, N_u);  
 
 end
