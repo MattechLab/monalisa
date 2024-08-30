f0f0e8afd2bee801c07882b2f1c4f374dffbab49
Neroxsis <helbing.dominik@gmail.com>
2024-08-30 12:53:36 +0200

    Add documentation & comments (combine image)

--- src/bmCoilSense/map/bmCoilSense_pinv.m
+++ src/bmCoilSense/map/bmCoilSense_pinv.m
@@ -1,13 +1,45 @@
-% Bastien Milani
-% CHUV and UNIL
-% Lausanne - Switzerland
-% May 2023
-
 function x = bmCoilSense_pinv(C, x0, n_u)
+% x = bmCoilSense_pinv(C, x0, n_u)
+%
+% This function performs image reconstruction using coil sensitivity
+% profiles and a pseudoinverse.
+%
+% Authors:
+%   Bastien Milani
+%   CHUV and UNIL
+%   Lausanne - Switzerland
+%   May 2023
+%
+% Contributors:
+%   Dominik Helbing (Documentation & Comments)
+%
+% Parameters:
+%   C (array): The coil sensitivity of each coil. Has the same amount of
+%    points as the data.
+%   x0 (array): The data acquired by each coil. Has the same amount of
+%    points as C.
+%   n_u (list): The size of the grid.
+%
+% Returns:
+%   x (array): The reconstructed image by combining the data of all coils.
+%    This is given in the block format.
+% 
+% Notes:
+%   The reconstruction is done by multiplying the pseudoinverse of the coil
+%   sensitivity profiles with the recorded data and summing all coils
+%   together, normalizing with the contribution from the different coils.
+%   This comes from x0 = C * x -> x = (C*) * x0 with C* being the
+%   conjugate.
 
+% Change to column format
 C       = single(bmColReshape(C, n_u)); 
 x0      = single(bmColReshape(x0, n_u)); 
+
+% Reconstructing the image by summing and normalizing the data acquired
+% with a pseudoinverse multiplication of the coil data.
 x       = sum(conj(C).*x0, 2)./sum(abs(C).^2, 2);
+
+% Change to block format
 x       = bmBlockReshape(x, n_u); 
 
 end
