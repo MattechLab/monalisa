0923fa97f6e38012cc0f2923d6af441a260960ba
Evelyn92 <evelynjia92@gmail.com>
2024-09-04 17:03:41 +0200

    update bmSteva

--- src/bmFourier123/solver_function/imDim/nonCartesian/bmSteva.m
+++ src/bmFourier123/solver_function/imDim/nonCartesian/bmSteva.m
@@ -4,18 +4,18 @@
 % May 2023
 
 function x = bmSteva(  x, z, u, y, ve, C, Gu, Gut, n_u, ...
-                                    delta, rho, nCGD, ve_max, ...
-                                    convCond, witnessInfo)
+    delta, rho, nCGD, ve_max, ...
+    nIter, witnessInfo)
 
 % initial -----------------------------------------------------------------
 myEps   = 10*eps('single'); % -------------------------------------------------- magic number
-    
+
 y               = single(y);   
 nCh             = size(y, 2);
 N_u             = double(int32(Gu.N_u(:)'));
 n_u             = double(n_u(:)');
 if isempty(n_u)
-   n_u = N_u;  
+n_u = N_u;  
 end
 nPt_u           = prod(n_u(:)); 
 imDim           = size(N_u(:), 1);  
@@ -23,10 +23,10 @@ dK_u            = double(single(Gu.d_u(:)'));
 dX_u            = single(  (1./single(dK_u))./single(N_u)  );
 
 if isempty(ve_max)
-   ve_max = max(ve(:));  
+ve_max = max(ve(:));  
 end
 
-[delta, rho]    = private_init_delta_rho(delta, rho, convCond); 
+[delta, rho]    = private_init_delta_rho(delta, rho, nIter); 
 
 HX              = single(  prod(dX_u(:))  );
 HZ              = single(  prod(dX_u(:))  );
@@ -38,125 +38,123 @@ KFC_conj        = single(bmKF_conj(conj(C), N_u, n_u, dK_u, nCh, Gu.kernel_type,
 x = single(bmColReshape(x, n_u));
 
 if isempty(z)
-    z = bmBackGradient(x, n_u, dX_u);
+z = bmBackGradient(x, n_u, dX_u);
 end
 if isempty(u)
-    u = bmZero([nPt_u, imDim], 'complex_single');
+u = bmZero([nPt_u, imDim], 'complex_single');
 end
 
-private_init_witnessInfo(witnessInfo, convCond, 'steva', n_u, N_u, dK_u, delta, rho, nCGD, ve_max); 
+private_init_witnessInfo(witnessInfo, nIter, 'steva', n_u, N_u, dK_u, delta, rho, nCGD, ve_max); 
 % END_initial -------------------------------------------------------------
 
 
 
 % ADMM loop ---------------------------------------------------------------
-while convCond.check()
-    
-    c = convCond.nIter_curr; 
+for c = 1:nIter
 
-    res_y_next   = y - bmShanna(x, Gu, KFC, n_u, 'MATLAB');
-    res_z_next   = (z - u) - bmBackGradient(x, n_u, dX_u);
-    
-    dagM_res_y_next  = (1/HX)*bmNakatsha(HY.*res_y_next, Gut, KFC_conj, true, n_u, 'MATLAB'); 
-    dagF_res_z_next  = rho(1, c)*(1/HX)*bmBackGradientT(HZ*res_z_next, n_u, dX_u);
-    
-    dagA_res_next   = dagM_res_y_next + dagF_res_z_next; 
-    p_next          = dagA_res_next; 
-    
-    sqn_dagA_res_next = real(   dagA_res_next(:)'*(HX*dagA_res_next(:))   );
-    
+res_y_next   = y - bmShanna(x, Gu, KFC, n_u, 'MATLAB');
+res_z_next   = (z - u) - bmBackGradient(x, n_u, dX_u);
 
-    for i = 1:nCGD
-        
-        res_y_curr   = res_y_next;
-        res_z_curr   = res_z_next;
-        sqn_dagA_res_curr = sqn_dagA_res_next; 
-        p_curr = p_next; 
-        
-        
-        if (sqn_dagA_res_curr < myEps)
-            break;
-        end
-        
-        
-        Mp_curr     = bmShanna(p_curr, Gu, KFC, n_u, 'MATLAB');
-        Fp_curr     = bmBackGradient(p_curr, n_u, dX_u);
-        
-        sqn_Mp_curr      = real(   Mp_curr(:)'*(HY(:).*Mp_curr(:))   );
-        sqn_Fp_curr      = real(   Fp_curr(:)'*(rho(1, c)*HZ*Fp_curr(:))   );
-        sqn_Ap_curr       = sqn_Mp_curr + sqn_Fp_curr;
-        
-        a   = sqn_dagA_res_curr/sqn_Ap_curr;
-        
-        x = x + a*p_curr;
-        
-        if i == nCGD
-            break;
-        end
-        
-        res_y_next          = res_y_curr - a*Mp_curr;
-        res_z_next          = res_z_curr - a*Fp_curr;
-        
-        dagM_res_y_next   = (1/HX)*bmNakatsha(HY.*res_y_next, Gut, KFC_conj, true, n_u, 'MATLAB');
-        dagF_res_z_next   = rho(1, c)*(1/HX)*bmBackGradientT(HZ*res_z_next, n_u, dX_u);
-        
-        dagA_res_next     = dagM_res_y_next + dagF_res_z_next;
-        sqn_dagA_res_next = real(   dagA_res_next(:)'*(HX*dagA_res_next(:))   );
-        
-        b = sqn_dagA_res_next/sqn_dagA_res_curr; 
-        
-        p_next           = dagA_res_next + b*p_curr;
+dagM_res_y_next  = (1/HX)*bmNakatsha(HY.*res_y_next, Gut, KFC_conj, true, n_u, 'MATLAB'); 
+dagF_res_z_next  = rho(1, c)*(1/HX)*bmBackGradientT(HZ*res_z_next, n_u, dX_u);
 
-    end
+dagA_res_next   = dagM_res_y_next + dagF_res_z_next; 
+p_next          = dagA_res_next; 
 
-    bGx_plus_u      = bmBackGradient(x, n_u, dX_u) + u; 
-    z               = bmProx_oneNorm(bGx_plus_u, delta(1, c)/rho(1, c)); 
-    u               = bGx_plus_u - z; 
-    
-    
-    % monitoring ----------------------------------------------------------
-    temp_r          = y - bmShanna(x, Gu, KFC, n_u, 'MATLAB');
-    R               = real(  temp_r(:)'*(HY(:).*temp_r(:))  ); 
-    
-    temp_r  = bmBackGradient(x, n_u, dX_u);
-    TV      = HZ*sum(abs(  real(temp_r(:))  )) + HZ*sum(abs(  imag(temp_r(:))  ));
-    
-    witnessInfo.param{9}(1, c)  = R; 
-    witnessInfo.param{10}(1, c) = TV; 
-    % END_monitoring ------------------------------------------------------
-    
-    
-    witnessInfo.watch(c, x, n_u, 'loop'); 
+sqn_dagA_res_next = real(   dagA_res_next(:)'*(HX*dagA_res_next(:))   );
+
+
+for i = 1:nCGD
+
+res_y_curr   = res_y_next;
+res_z_curr   = res_z_next;
+sqn_dagA_res_curr = sqn_dagA_res_next; 
+p_curr = p_next; 
+
+
+if (sqn_dagA_res_curr < myEps)
+break;
+end
+
+
+Mp_curr     = bmShanna(p_curr, Gu, KFC, n_u, 'MATLAB');
+Fp_curr     = bmBackGradient(p_curr, n_u, dX_u);
+
+sqn_Mp_curr      = real(   Mp_curr(:)'*(HY(:).*Mp_curr(:))   );
+sqn_Fp_curr      = real(   Fp_curr(:)'*(rho(1, c)*HZ*Fp_curr(:))   );
+sqn_Ap_curr       = sqn_Mp_curr + sqn_Fp_curr;
+
+a   = sqn_dagA_res_curr/sqn_Ap_curr;
+
+x = x + a*p_curr;
+
+if i == nCGD
+break;
+end
+
+res_y_next          = res_y_curr - a*Mp_curr;
+res_z_next          = res_z_curr - a*Fp_curr;
+
+dagM_res_y_next   = (1/HX)*bmNakatsha(HY.*res_y_next, Gut, KFC_conj, true, n_u, 'MATLAB');
+dagF_res_z_next   = rho(1, c)*(1/HX)*bmBackGradientT(HZ*res_z_next, n_u, dX_u);
+
+dagA_res_next     = dagM_res_y_next + dagF_res_z_next;
+sqn_dagA_res_next = real(   dagA_res_next(:)'*(HX*dagA_res_next(:))   );
+
+b = sqn_dagA_res_next/sqn_dagA_res_curr; 
+
+p_next           = dagA_res_next + b*p_curr;
+
+end
+
+bGx_plus_u      = bmBackGradient(x, n_u, dX_u) + u; 
+z               = bmProx_oneNorm(bGx_plus_u, delta(1, c)/rho(1, c)); 
+u               = bGx_plus_u - z; 
+
+
+% monitoring ----------------------------------------------------------
+temp_r          = y - bmShanna(x, Gu, KFC, n_u, 'MATLAB');
+R               = real(  temp_r(:)'*(HY(:).*temp_r(:))  ); 
+
+temp_r  = bmBackGradient(x, n_u, dX_u);
+TV      = HZ*sum(abs(  real(temp_r(:))  )) + HZ*sum(abs(  imag(temp_r(:))  ));
+
+witnessInfo.param{9}(1, c)  = R; 
+witnessInfo.param{10}(1, c) = TV; 
+% END_monitoring ------------------------------------------------------
+
+
+witnessInfo.watch(c, x, n_u, 'loop'); 
 end
 % END_ADMM loop -----------------------------------------------------------
 
 % final -------------------------------------------------------------------
-witnessInfo.watch(convCond.nIter_curr, x, n_u, 'final'); 
+witnessInfo.watch(c, x, n_u, 'final'); 
 x = bmBlockReshape(x, n_u);
 % END_final ---------------------------------------------------------------
 
 end
 
-function [delta, rho] = private_init_delta_rho(delta, rho, convCond)
+function [delta, rho] = private_init_delta_rho(delta, rho, nIter)
 
 rho             = single(  abs(rho(:))  );
 delta           = single(  abs(delta(:))  );
 if size(delta, 1) == 1
-    delta   = linspace(delta, delta, convCond.nIter_max);
+delta   = linspace(delta, delta, nIter);
 elseif size(delta, 1) == 2
-    delta   = linspace(delta(1, 1), delta(2, 1), convCond.nIter_max);
+delta   = linspace(delta(1, 1), delta(2, 1), nIter);
 end
 delta = delta(:)';
 if size(rho, 1) == 1
-    rho     = linspace(rho,     rho, convCond.nIter_max);
+rho     = linspace(rho,     rho, nIter);
 elseif size(rho, 1) == 2
-    rho     = linspace(rho(1, 1),     rho(2, 1), convCond.nIter_max);
+rho     = linspace(rho(1, 1),     rho(2, 1), nIter);
 end
 rho = rho(:)';
 
 end
 
-function private_init_witnessInfo(witnessInfo, convCond, argName, n_u, N_u, dK_u, delta, rho, nCGD, ve_max)
+function private_init_witnessInfo(witnessInfo, nIter, argName, n_u, N_u, dK_u, delta, rho, nCGD, ve_max)
 
 witnessInfo.param_name{1}    = 'recon_name'; 
 witnessInfo.param{1}         = argName; 
@@ -183,9 +181,9 @@ witnessInfo.param_name{8}    = 've_max';
 witnessInfo.param{8}         = ve_max;
 
 witnessInfo.param_name{9}    = 'residu'; 
-witnessInfo.param{9}         = zeros(1, convCond.nIter_max); 
+witnessInfo.param{9}         = zeros(1, nIter); 
 
 witnessInfo.param_name{10}   = 'total_variation'; 
-witnessInfo.param{10}        = zeros(1, convCond.nIter_max); 
+witnessInfo.param{10}        = zeros(1, nIter); 
 
 end
