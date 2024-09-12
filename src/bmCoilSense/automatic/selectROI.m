af07e8cd11181816ffdc03c8d2c214cf7f84f68a
Neroxsis <helbing.dominik@gmail.com>
2024-09-12 11:08:47 +0200

    Add option to manualy input numbers

--- src/bmCoilSense/automatic/selectROI.m
+++ src/bmCoilSense/automatic/selectROI.m
@@ -1,5 +1,5 @@
 function coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
-% coordinates = selectROI(dataRMS, dataMIP, N_u, boxPos)
+% coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
 % 
 % This function allows to manually set the bounding box around the region
 % of interest (ROI) for every dimension of the image in dataRMS and
@@ -16,9 +16,12 @@ function coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
 %   dataRMS (array): Data for RMS image. Has to be data of a 3D image.
 %   dataMIP (array): Data for MIP image. Has to be data of a 3D image.
 %   N_u (list): Contains the size of the data in every dimension.
-%   varargin{1}: Array that contains the guessed position of the bounding
+%   varargin{1}: Flag; allows changing of coordinates with input fields.
+%    Default value is false.
+%   varargin{2}: Array that contains the guessed position of the bounding
 %    box. Has to have the structure [xMin, xWidth; yMin, yWidth; zMin,
-%    zWidth]. Default value is [2, 2, min(N_u)-2, min(N_u)-2]
+%    zWidth]. Default value is 
+%    [2, min(N_u)-4; 2, min(N_u)-4; 2, min(N_u)-4]
 %
 % Returns:
 %   coordinates (array): The updated coordinates of the bounding box
@@ -27,7 +30,7 @@ function coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
 
     %% Initialize arguments
     % Extract optional arguments
-    boxPos = bmVarargin(varargin);
+    [autoFlag, boxPos]  = bmVarargin(varargin);
 
     % Ensure correct size
     N_u = N_u(:)';
@@ -36,7 +39,12 @@ function coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
 
     % Set default value if position is not given
     if isempty(boxPos)
-        boxPos = [2, 2, min(N_u)-2, min(N_u)-2];
+        boxPos = repmat([2, min(N_u)-4], [3,1]);
+    end
+
+    % Set default value for flag
+    if isempty(autoFlag)
+        autoFlag = false;
     end
     
     % Sum over every dimension and normalize
@@ -91,16 +99,17 @@ function coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
     title(tMIP, "MIP Values Summed Along Each Axis");
     
     % Add a confirmation button
-    bRMS = uicontrol(figRMS ,'Style', 'pushbutton', ...
+    uicontrol(figRMS, 'Style', 'pushbutton', ...
         'String', 'Confirm Selection', ...
         'Units', 'normalized', 'Position', [0.4 0.04 0.2 0.06], ...
-        'Callback', @(src, event) confirmSelection(figRMS, tRMS, tMIP));
+        'Callback', @(src, event) confirmSelection(figRMS));
     
-    bMIP = uicontrol(figMIP ,'Style', 'pushbutton', ...
+    uicontrol(figMIP, 'Style', 'pushbutton', ...
         'String', 'Confirm Selection', ...
         'Units', 'normalized', 'Position', [0.4 0.04 0.2 0.06], ...
-        'Callback', @(src, event) confirmSelection(figRMS, tRMS, tMIP));
+        'Callback', @(src, event) confirmSelection(figRMS));
     
+    % Add Update on move option (checkbox)
     cbxR = uicontrol(figRMS, 'Style', 'checkbox', ...
         'String', ' Update on move', ...
         'Units', 'normalized', 'Position', [0.04 0.04 0.2 0.06]);
@@ -108,7 +117,65 @@ function coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
     cbxM = uicontrol(figMIP, 'Style', 'checkbox', ...
         'String', ' Update on move', ...
         'Units', 'normalized', 'Position', [0.04 0.04 0.2 0.06]);
+
+    % Add text fields to show min and max values
+    % Make them editable if autoFlag is false
+    fStyle = 'text';
+    if ~autoFlag
+        fStyle = 'edit';
+    end
+
+    % XMin
+    eXMinR = uicontrol(figRMS, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.425 0.15 0.05 0.05], 'Tooltip', 'XMin of the mask', ...
+        'String', num2str(boxPos(1,1)));
+    eXMinM = uicontrol(figMIP, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.425 0.15 0.05 0.05], 'Tooltip', 'XMin of the mask', ...
+        'String', num2str(boxPos(1,1)));
     
+    % XMax
+    xMaxStr = num2str(boxPos(1,1) + boxPos(1,2));
+    eXMaxR = uicontrol(figRMS, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.525 0.15 0.05 0.05], 'Tooltip', 'XMax of the mask', ...
+        'String', xMaxStr);
+    eXMaxM = uicontrol(figMIP, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.525 0.15 0.05 0.05], 'Tooltip', 'XMax of the mask', ...
+        'String', xMaxStr);
+    
+    % YMin
+    eYMinR = uicontrol(figRMS, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.715 0.15 0.05 0.05], 'Tooltip', 'YMin of the mask', ...
+        'String', num2str(boxPos(2,1)));
+    eYMinM = uicontrol(figMIP, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.715 0.15 0.05 0.05], 'Tooltip', 'YMin of the mask', ...
+        'String', num2str(boxPos(2,1)));
+    
+    % YMax
+    yMaxStr = num2str(boxPos(2,1) + boxPos(2,2));
+    eYMaxR = uicontrol(figRMS, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.815 0.15 0.05 0.05], 'Tooltip', 'YMax of the mask', ...
+        'String', yMaxStr);
+    eYMaxM = uicontrol(figMIP, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.815 0.15 0.05 0.05], 'Tooltip', 'YMax of the mask', ...
+        'String', yMaxStr);
+    
+    % ZMin
+    eZMinR = uicontrol(figRMS, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.135 0.15 0.05 0.05], 'Tooltip', 'ZMin of the mask', ...
+        'String', num2str(boxPos(3,1)));
+    eZMinM = uicontrol(figMIP, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.135 0.15 0.05 0.05], 'Tooltip', 'ZMin of the mask', ...
+        'String', num2str(boxPos(3,1)));
+    
+    % ZMax
+    zMaxStr = num2str(boxPos(3,1) + boxPos(3,2));
+    eZMaxR = uicontrol(figRMS, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.235 0.15 0.05 0.05], 'Tooltip', 'ZMax of the mask', ...
+        'String', zMaxStr);
+    eZMaxM = uicontrol(figMIP, 'Style', fStyle, 'Units', 'normalized', ...
+        'Position', [0.235 0.15 0.05 0.05], 'Tooltip', 'ZMax of the mask', ...
+        'String', zMaxStr);
+
     
     %% Plot data
     % Plot YZ view for RMS
@@ -179,23 +246,63 @@ function coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
 
     % Create movable rectangle on image
     zRectM = images.roi.Rectangle(axMIPz,'Position', nPos);
+
+
+    %% Create struct for Call back functions
+    components = struct(...
+        'xRectR', xRectR, ...
+        'yRectR', yRectR, ...
+        'zRectR', zRectR, ...
+        'xRectM', xRectM, ...
+        'yRectM', yRectM, ...
+        'zRectM', zRectM, ...
+        'eXMinR', eXMinR, ...
+        'eXMaxR', eXMaxR, ...
+        'eYMinR', eYMinR, ...
+        'eYMaxR', eYMaxR, ...
+        'eZMinR', eZMinR, ...
+        'eZMaxR', eZMaxR, ...
+        'eXMinM', eXMinM, ...
+        'eXMaxM', eXMaxM, ...
+        'eYMinM', eYMinM, ...
+        'eYMaxM', eYMaxM, ...
+        'eZMinM', eZMinM, ...
+        'eZMaxM', eZMaxM ...
+    );
+
+
+    %% Add callback functions to edit fields if autoFlag is false
+    if ~autoFlag
+        eXMinR.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eXMaxR.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eYMinR.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eYMaxR.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eZMinR.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eZMaxR.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eXMinM.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eXMaxM.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eYMinM.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eYMaxM.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eZMinM.Callback = @(src, evnt)updateBorder(src, components, N_u);
+        eZMaxM.Callback = @(src, evnt)updateBorder(src, components, N_u);
+    end
     
 
     %% Add listeners to rectangles 
     % Listeners update boxes after changing them in the image and letting
     % go
     el1 = addlistener(xRectR, "ROIMoved", @(src, evnt)updateBorder(src, ...
-        xRectR, yRectR, zRectR, xRectM, yRectM, zRectM));
+        components, N_u));
     el2 = addlistener(yRectR, "ROIMoved", @(src, evnt)updateBorder(src, ...
-        xRectR, yRectR, zRectR, xRectM, yRectM, zRectM));
+        components, N_u));
     el3 = addlistener(zRectR, "ROIMoved", @(src, evnt)updateBorder(src, ...
-        xRectR, yRectR, zRectR, xRectM, yRectM, zRectM));
+        components, N_u));
     el4 = addlistener(xRectM, "ROIMoved", @(src, evnt)updateBorder(src, ...
-        xRectR, yRectR, zRectR, xRectM, yRectM, zRectM));
+        components, N_u));
     el5 = addlistener(yRectM, "ROIMoved", @(src, evnt)updateBorder(src, ...
-        xRectR, yRectR, zRectR, xRectM, yRectM, zRectM));
-    el6 = addlistener(zRectR, "ROIMoved", @(src, evnt)updateBorder(src, ...
-        xRectR, yRectM, zRectR, xRectM, yRectM, zRectM));
+        components, N_u));
+    el6 = addlistener(zRectM, "ROIMoved", @(src, evnt)updateBorder(src, ...
+        components, N_u));
     
     % Combine listeners into array for easier access
     listeners = [el1, el2, el3, el4, el5, el6];
@@ -216,9 +323,9 @@ function coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
 
     % Update with new values if figures were not closed
     if ishandle(figRMS) && ishandle(figMIP)
-        coordinates = borderExtraction(xRectR, yRectR, zRectR, xRectM, ...
-            yRectM, zRectM);
-
+        coordinates = borderExtraction(components, N_u);
+        delete(figRMS);
+        delete(figMIP);
     else
         disp("Use confirm to use the new values. " + ...
              "Don't close the figures!");
@@ -229,104 +336,191 @@ function coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
 
 
     %% Nested functions
-    function coordinates = borderExtraction(xRectR, yRectR, zRectR, ...
-            xRectM, yRectM, zRectM)
+    function coordinates = borderExtraction(comp, N_u)
         % This function extracts the border values, makes the rectangles 
         % non interactive and returns the border values as a [3,2] array
+        % comp is the structure containing all fields and rectangles
+
+        % Get array size
+        Nx = N_u(1);
+        Ny = N_u(2);
+        Nz = N_u(3);
     
         % Extract borders
-        yMin = xRectR.Vertices(1,2);
-        yMax = xRectR.Vertices(3,2);
-        zMin = xRectR.Vertices(1,1);
-        zMax = xRectR.Vertices(3,1);
-        xMin = yRectR.Vertices(1,1);
-        xMax = yRectR.Vertices(3,1);
+        yMin = comp.xRectR.Vertices(1,2);
+        yMax = comp.xRectR.Vertices(3,2);
+        zMin = comp.xRectR.Vertices(1,1);
+        zMax = comp.xRectR.Vertices(3,1);
+        xMin = comp.yRectR.Vertices(1,1);
+        xMax = comp.yRectR.Vertices(3,1);
         
         
-        % Scale from [0.5, 48.5] to [1, 48] and round
-        xMin = round(1 + ((xMin - 0.5) / 48) * 47);
-        xMax = round(1 + ((xMax - 0.5) / 48) * 47);
-        yMin = round(1 + ((yMin - 0.5) / 48) * 47);
-        yMax = round(1 + ((yMax - 0.5) / 48) * 47);
-        zMin = round(1 + ((zMin - 0.5) / 48) * 47);
-        zMax = round(1 + ((zMax - 0.5) / 48) * 47);
+        % Scale from [0.5, N_u + 0.5] to [1, N_u] and round
+        xMin = round(1 + ((xMin - 0.5) / Nx) * (Nx - 1));
+        xMax = round(1 + ((xMax - 0.5) / Nx) * (Nx - 1));
+        yMin = round(1 + ((yMin - 0.5) / Ny) * (Ny - 1));
+        yMax = round(1 + ((yMax - 0.5) / Ny) * (Ny - 1));
+        zMin = round(1 + ((zMin - 0.5) / Nz) * (Nz - 1));
+        zMax = round(1 + ((zMax - 0.5) / Nz) * (Nz - 1));
     
         % Make squares non interactive
-        xRectR.InteractionsAllowed = 'none';
-        yRectR.InteractionsAllowed = 'none';
-        zRectR.InteractionsAllowed = 'none';
-        xRectM.InteractionsAllowed = 'none';
-        yRectM.InteractionsAllowed = 'none';
-        zRectM.InteractionsAllowed = 'none';
+        comp.xRectR.InteractionsAllowed = 'none';
+        comp.yRectR.InteractionsAllowed = 'none';
+        comp.zRectR.InteractionsAllowed = 'none';
+        comp.xRectM.InteractionsAllowed = 'none';
+        comp.yRectM.InteractionsAllowed = 'none';
+        comp.zRectM.InteractionsAllowed = 'none';
     
         coordinates = [xMin, xMax; yMin, yMax; zMin, zMax];
     end
 
 
-    function confirmSelection(fig, t1, t2)
+    function confirmSelection(fig)
         % This function resumes the code execution
-        if ishandle(fig)
-            % Show in title that the code execution continues
-            title(t1, 'Confirmed');
-            title(t2, 'Confirmed');
-            
+        if ishandle(fig)            
             % Resume execution
             uiresume(fig);
         end
     end
 
 
-    function updateBorder(src, xRectR, yRectR, zRectR, xRectM, yRectM, ...
-            zRectM)
-        % This function updates all squares if one is modified
+    function updateBorder(src, comp, N_u)
+        % This function updates all squares if one is modified. comp is the
+        % structure containing all fields and rectangles.
     
-        % Ensure the source of the event is actually a rectangle
-        if ~isa(src,'images.roi.Rectangle')
+        % Ensure the source of the event is actually a rectangle or edit
+        % field
+        if isa(src,'images.roi.Rectangle')
+            % Read position of updated square
+            pos = src.Position;
+        
+        elseif isa(src, 'matlab.ui.control.UIControl')
+            % Read new extrema
+            ext = str2double(src.String);
+
+            % If it is not a number it will be returned as nan
+            if isnan(ext)
+                % Overwrite field with coordinates from rectangles
+                updateFields(comp);
+                return
+            end
+
+        else
             return
         end
         
-        % Read positions all squares (xRectR = xRectM sizewise)
-        posX = xRectR.Position;
-        posY = yRectR.Position;
-        posZ = zRectR.Position;
-        
-        % Read position of updated square
-        pos = src.Position;
+        % Read all squares position's (xRectR = xRectM sizewise)
+        posX = comp.xRectR.Position;
+        posY = comp.yRectR.Position;
+        posZ = comp.zRectR.Position;
     
         switch src
-            case {xRectR, xRectM}
+            case {comp.xRectR, comp.xRectM}
                 % Update position lists with the new values
                 posX = pos;
                 posY = [posY(1), pos(1), posY(3), pos(3)];
                 posZ = [pos(2), posZ(2), pos(4), posZ(4)];
     
-            case {yRectR, yRectM}
+            case {comp.yRectR, comp.yRectM}
                 % Update position lists with the new values
                 posX = [pos(2), posX(2), pos(4), posX(4)];
                 posY = pos;
                 posZ = [posZ(1), pos(1), posZ(3), pos(3)];
     
-            case {zRectR, zRectM}
+            case {comp.zRectR, comp.zRectM}
                 % Update position lists with the new values
                 posX = [posX(1), pos(1), posX(3), pos(3)];
                 posY = [pos(2), posY(2), pos(4), posY(4)];
                 posZ = pos;
+
+            case {comp.eXMinR, comp.eXMinM}
+                % Clip input to valid range
+                ext = min(max(0.5, ext), N_u(1) + 0.5); 
+                
+                % Update position lists with the new values
+                posY = [ext, posY(2), posY(3) + posY(1) - ext, posY(4)];
+                posZ = [posZ(1), ext, posZ(3), posZ(4) + posZ(2) - ext];
+
+            case {comp.eYMinR, comp.eYMinM}
+                % Clip input to valid range
+                ext = min(max(0.5, ext), N_u(2) + 0.5); 
+
+                % Update position lists with the new values
+                posX = [posX(1), ext, posX(3), posX(4) + posX(2) - ext];
+                posZ = [ext, posZ(2), posZ(3) + posZ(1) - ext, posZ(4)];
+
+            case {comp.eZMinR, comp.eZMinM}
+                % Clip input to valid range
+                ext = min(max(0.5, ext), N_u(3) + 0.5); 
+
+                % Update position lists with the new values
+                posX = [ext, posX(2), posX(3) + posX(1) - ext, posX(4)];
+                posY = [posY(1), ext, posY(3), posY(4) + posY(2) - ext];
+
+            case {comp.eXMaxR, comp.eXMaxM}
+                % Clip input to valid range and calculate width
+                ext = min(max(0.5, ext), N_u(1) + 0.5);
+                ext = ext - posY(1);                 
+
+                % Update position lists with the new values
+                posY = [posY(1), posY(2), ext, posY(4)];
+                posZ = [posZ(1), posZ(2), posZ(3), ext];
+
+            case {comp.eYMaxR, comp.eYMaxM}
+                % Clip input to valid range and calculate width
+                ext = min(max(0.5, ext), N_u(1) + 0.5);
+                ext = ext - posZ(1);
+
+                % Update position lists with the new values
+                posX = [posX(1), posX(2), posX(3), ext];
+                posZ = [posZ(1), posZ(2), ext, posZ(4)];
+
+            case {comp.eZMaxR, comp.eZMaxM}
+                % Clip input to valid range and calculate width
+                ext = min(max(0.5, ext), N_u(1) + 0.5);
+                ext = ext - posX(1);
+
+                % Update position lists with the new values
+                posX = [posX(1), posX(2), ext, posX(4)];
+                posY = [posY(1), posY(2), posY(3), ext];
     
             otherwise
         end
     
         % Override positions of all squares (M and R have the same
         % squares)
-        xRectR.Position = posX;
-        yRectR.Position = posY;
-        zRectR.Position = posZ;
+        comp.xRectR.Position = posX;
+        comp.yRectR.Position = posY;
+        comp.zRectR.Position = posZ;
     
-        xRectM.Position = posX;
-        yRectM.Position = posY;
-        zRectM.Position = posZ;
+        comp.xRectM.Position = posX;
+        comp.yRectM.Position = posY;
+        comp.zRectM.Position = posZ;
+
+        % Update text fields
+        updateFields(comp);
     
     end
 
+    
+    function updateFields(comp)
+        % This functions updates the fields with the value from the
+        % rectangles. Comp is the structure containing all fields and
+        % rectangles.
+
+        comp.eXMinR.String = num2str(comp.yRectR.Vertices(1,1));
+        comp.eXMaxR.String = num2str(comp.yRectR.Vertices(3,1));
+        comp.eYMinR.String = num2str(comp.xRectR.Vertices(1,2));
+        comp.eYMaxR.String = num2str(comp.xRectR.Vertices(3,2));
+        comp.eZMinR.String = num2str(comp.xRectR.Vertices(1,1));
+        comp.eZMaxR.String = num2str(comp.xRectR.Vertices(3,1));
+        comp.eXMinM.String = comp.eXMinR.String;
+        comp.eXMaxM.String = comp.eXMaxR.String;
+        comp.eYMinM.String = comp.eYMinR.String;
+        comp.eYMaxM.String = comp.eYMaxR.String;
+        comp.eZMinM.String = comp.eZMinR.String;
+        comp.eZMaxM.String = comp.eZMaxR.String;
+    end
 
     function updateListeners(src, cbxR, cbxM, listeners)
         % This function changes the listener event between MovingROI and
@@ -355,7 +549,7 @@ function coordinates = selectROI(dataRMS, dataMIP, N_u, varargin)
             listeners(i).EventName = eventName;
         end
     end
-
+    
 
 end
 
