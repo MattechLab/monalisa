e4ae91f94bf02943cc671fe359bc9f39010f5e2f
Neroxsis <helbing.dominik@gmail.com>
2024-09-12 11:09:15 +0200

    Add documentation & comments

--- src/bmArrayUtility/bmZero.m
+++ src/bmArrayUtility/bmZero.m
@@ -1,19 +1,35 @@
-% Bastien Milani
-% CHUV and UNIL
-% Lausanne - Switzerland
-% May 2023
-
-
-% varargin is the size of the cell array, in case we want the output z to
-% be a cell array. 
-
 function z = bmZero(argSize, argType, varargin)
+% z = bmZero(argSize, argType, varargin)
+%
+% This function creates a zero array of a given size and given type. Can
+% also create a cell array containing zero arrays of the given size.
+%
+% Authors:
+%   Bastien Milani
+%    CHUV and UNIL
+%   Lausanne - Switzerland
+%   May 2023
+%
+% Contributors:
+%   Dominik Helbing (Documentation & Comments)
+%   MattechLab 2024
+%
+% Parameters:
+%   argSize (list): The size of the zero array.
+%   argType (char): The type of the zero array. Can be 'real_double',
+%    'real_single', 'complex_double' and 'complex_single'.
+%   varargin{1}: List containing the size of the cell array if the
+%    result should be a cell array.
 
+% Extract optional arguments
 frame_size = bmVarargin(varargin); 
+
 if ~isempty(frame_size)
+    % Create cell array
     z = cell(frame_size);
     z = z(:);
     for i = 1:size(z(:), 1)
+        % Create zero array in every cell
         z{i} = bmZero(argSize, argType);
     end
     z = reshape(z, frame_size);
@@ -21,6 +37,7 @@ if ~isempty(frame_size)
 end
 
 z = []; 
+% Create zero array of different types
 if strcmp(argType, 'real_double')
     z = zeros(argSize, 'double'); 
 elseif strcmp(argType, 'complex_double')
