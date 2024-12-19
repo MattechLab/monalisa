9640397b815959093a0bcd75f7d6487573204150
MauroLeidi <MauroLeidi@users.noreply.github.com>
2024-12-19 13:22:24 +0000

    deploy: fe7881c19f5eb36f9e801491bbf0c4c818254ef3

--- demo/function_demo/function_tutorial_1/updatePlot.m
+++
@@ -1,7 +0,0 @@
-function updatePlot(plotHandle, selectedBin, sequentialBinningMask, timestampMs)
-    % Update the plot's YData to the selected bin's data
-    plotHandle.YData = sequentialBinningMask(selectedBin, :);
-    
-    % Update the title to reflect the selected bin
-    plotHandle.Parent.Title.String = ['Sequential Binning Mask: Bin ' num2str(selectedBin)];
-end
