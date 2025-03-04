77628c57dfe77769bd1f8784791e015dd53502f6
PC-JAIMEBARRAN\Jaime <jaimebarran@gmail.com>
2025-03-04 13:14:53 +0100

    fix: make.bat for Windows

--- docs/make.bat
+++ docs/make.bat
@@ -2,35 +2,49 @@
 
 pushd %~dp0
 
-REM Run codespell to check for typos with an ignore list
-codespell -i 3 -I codespell_ignore.txt ./rst
+REM Run codespell to check for typos interactively
+IF "%1"=="check_typos" (
+    codespell -w -i 3 -I .\rst\codespell_ignore.txt .\rst
+    EXIT /B 0
+)
 
 REM Run generate_rst.py before building the documentation
-python ./rst/generate_rst.py
-
-if "%SPHINXBUILD%" == "" (
-    set SPHINXBUILD=sphinx-build
+IF "%1"=="generate" (
+    python .\rst\generate_rst.py
+    EXIT /B 0
 )
-set SOURCEDIR=./rst
-set BUILDDIR=_build
 
+REM Set Sphinx build variables
+IF "%SPHINXBUILD%"=="" (
+    SET SPHINXBUILD=sphinx-build
+)
+SET SOURCEDIR=.\rst
+SET BUILDDIR=_build
+
+REM Check if Sphinx is installed
 %SPHINXBUILD% >NUL 2>NUL
-if errorlevel 9009 (
-    echo.
-    echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-    echo.installed, then set the SPHINXBUILD environment variable to point
-    echo.to the full path of the 'sphinx-build' executable. Alternatively you
-    echo.may add the Sphinx directory to PATH.
-    echo.
-    echo.If you don't have Sphinx installed, grab it from
-    echo.https://www.sphinx-doc.org/
-    exit /b 1
+IF ERRORLEVEL 9009 (
+    ECHO.
+    ECHO The 'sphinx-build' command was not found. Make sure you have Sphinx
+    ECHO installed, then set the SPHINXBUILD environment variable to point
+    ECHO to the full path of the 'sphinx-build' executable. Alternatively you
+    ECHO may add the Sphinx directory to PATH.
+    ECHO.
+    ECHO If you don't have Sphinx installed, grab it from
+    ECHO https://www.sphinx-doc.org/
+    EXIT /B 1
 )
 
-if "%1" == "" goto help
+REM Default to showing help if no argument is provided
+IF "%1"=="" GOTO help
 
+REM Run codespell and generate before building documentation
+codespell -w -i 3 -I .\rst\codespell_ignore.txt .\rst
+python .\rst\generate_rst.py
+
+REM Build the specified Sphinx target
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
-goto end
+GOTO end
 
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
