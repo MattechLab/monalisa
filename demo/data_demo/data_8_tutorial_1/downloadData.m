077fcdcc0e34537657ae64732f2d268ed787314b
MauroLeidi <mauro.leidi@hotmail.com>
2026-01-12 11:10:35 +0100

    bugfix tutorialdownload

--- demo/data_demo/data_8_tutorial_1/downloadData.m
+++ demo/data_demo/data_8_tutorial_1/downloadData.m
@@ -1,10 +1,11 @@
 function downloadData()
-%DOWNLOADDATA Download tutorial data from Google Drive
-%   Cross-platform: Windows / macOS / Linux
+%DOWNLOADDATA Download tutorial data using curl/wget via system()
+% Works on Windows / macOS / Linux
 
     fprintf('Downloading tutorial data...\n');
 
     scriptDir = fileparts(mfilename('fullpath'));
+    cd(scriptDir);
 
     files = {
         '11p1lUw4pcj_xp1kPuKv3LO_cfcyYlnw_', 'bodyCoil.dat'
@@ -12,22 +13,40 @@ function downloadData()
         '1dd8I74Hy4Hb97SF-fHBIkrZP_JzwpMa0', 'surfaceCoil.dat'
     };
 
-    opts = weboptions('Timeout', 120);
-
     for i = 1:size(files,1)
         fileId  = files{i,1};
-        outFile = fullfile(scriptDir, files{i,2});
-        url     = sprintf('https://drive.google.com/uc?export=download&id=%s', fileId);
+        outFile = files{i,2};
 
-        fprintf('→ %s\n', files{i,2});
+        fprintf('→ %s\n', outFile);
 
-        try
-            websave(outFile, url, opts);
+        cmd = buildDownloadCommand(fileId, outFile);
+
+        [status, out] = system(cmd);
+
+        if status == 0 && exist(outFile,'file')
             fprintf('  ✓ done\n');
-        catch ME
-            fprintf('  ✗ failed: %s\n', ME.message);
+        else
+            fprintf('  ✗ failed\n');
+            disp(out);
         end
     end
 
     fprintf('\nAll downloads completed.\n');
+end
+
+
+function cmd = buildDownloadCommand(fileId, outFile)
+%BUILD DOWNLOAD COMMAND FOR CURRENT OS
+
+    url = sprintf( ...
+        'https://drive.usercontent.google.com/download?id=%s&confirm=xxx', ...
+        fileId);
+
+    if ispc
+        % Windows: curl is available by default (Win10+)
+        cmd = sprintf('curl -L "%s" -o "%s"', url, outFile);
+    else
+        % macOS / Linux
+        cmd = sprintf('curl -L "%s" -o "%s"', url, outFile);
+    end
 end
